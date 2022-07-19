3d998a89738aab56eaea709baec711e36d0f29e7
Elif Sude Bulut <c1928007@student.cankaya.edu.tr>
2022-07-19 13:45:36 +0200

    Lesson 3:Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Elif';
+
+  items = [
+    { description: 'Breakfast', action: 'No' },
+    { description: 'Sports', action: 'No' },
+    { description: 'Paying Bills', action: 'Yes' },
+    { description: 'Go to the Cinema', action: 'No' },
+  ];
 }
