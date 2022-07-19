a00452a132d46ea479caefde83a7607c3245879c
Elif Sude Bulut <c1928007@student.cankaya.edu.tr>
2022-07-19 15:34:13 +0200

    5-Last Version of the To-Do List Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model, TodoItem } from './model';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +8,22 @@ import { Model } from './model';
 })
 export class AppComponent {
   model = new Model();
+  isDisplay = false;
 
   getName() {
     return this.model.user;
   }
 
   getItems() {
+    if (this.isDisplay) {
+      return this.model.items;
+    }
     return this.model.items.filter((item) => !item.action);
+  }
+
+  addItem(value: any) {
+    if (value != '') {
+      this.model.items.push(new TodoItem(value, false));
+    }
   }
 }
